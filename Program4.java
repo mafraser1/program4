55aaea85a823f95b999fad54e209c2b0600a3f9b
mafraser1 <mafraser@uncg.edu>
2016-03-29 14:05:09 -0400

    Classes complete Main left to complete Still needs comments

---
+++ Program4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Program4{
+    public static void main(String[] args) {
+        SpellChecker spellCheck = new SpellChecker();
+        
+   
+}
