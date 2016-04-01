d03dd3d85093d56ef20fd7733ee3973419590c4f
mafraser1 <mafraser@uncg.edu>
2016-03-31 21:54:07 -0400

    Final changes

--- SpellChecker.java
+++ SpellChecker.java
@@ -1,4 +1,8 @@
-
+/*
+Author: Maxwell Fraser
+The spellchecker class, makes a dictionary of words and checks for any words
+not in the dictionary.
+ */
 import java.util.Scanner;
 import java.io.*;
 
@@ -12,24 +16,28 @@ public class SpellChecker {
         size = 0;
     }
 
+    //uses the passed file as a scanner, and creates a doubly linked bag uses it's data
     public boolean setDictionary(File correctWordFile) {
-        try{
-        Scanner inFile = new Scanner(correctWordFile);
-        while(inFile.hasNext()){
-            correctWords.add(inFile.nextLine());
-        }
-        }
-        catch(FileNotFoundException e){
+        try {
+            Scanner inFile = new Scanner(correctWordFile);
+            while (inFile.hasNext()) {
+                String input = inFile.nextLine().toUpperCase();
+                correctWords.add(input);
+
+            }
+            inFile.close();
+        } catch (FileNotFoundException e) {
             return false;
         }
-
         return true;
     }
 
+    //checks to see if a word is contained in the user created dictionary
     public boolean checkSpelling(String word) {
         return correctWords.contains(word);
     }
 
+    //returns the size of the dictionary
     public int count() {
         return size;
     }
